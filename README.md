e367470d11d37b395bde2acf905428bec60be9f7
Robert Peters <robert.peters@usofrob.com>
2016-12-17 20:11:33 -0500

    Initial environment setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ReversePolishNotationConverter
 Initial commit
+
+## Environment requirements
+*	Ubuntu 14.04 or 16.04
+*	Run install script (./scripts/installEnvironment.sh)
---
+++ scripts/installEnvironment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function showError {
+	echo "Error during $1"
+	exit 1
+}
+
+# I wanted to use plant uml, but this project might be too simple to be worth it.
+# mkdir -p bin/ || showError "Creating bin directory"
+# wget http://sourceforge.net/projects/plantuml/files/plantuml.jar/download -O bin/plantuml.jar || showError "Downloading plantuml.jar"
+
+sudo apt-get install check || showError "Installing check"
