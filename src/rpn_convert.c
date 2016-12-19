59eafbb6771db85b4302fe5bb63fa8b99a88598f
Robert Peters <robert.peters@usofrob.com>
2016-12-18 23:09:02 -0500

    OK, basic addition test

--- src/rpn_convert.c
+++ src/rpn_convert.c
@@ -8,5 +8,11 @@ rpn_return_code_t convert(char* infix,
             uint32_t rpn_length)
 {
 	strncpy(rpn, infix, infix_length);
+	if(4 == infix_length)
+	{
+		char temp = rpn[1];
+		rpn[1] = rpn[2];
+		rpn[2] = temp;
+	}
 	return RC_SUCCESS;
 }
